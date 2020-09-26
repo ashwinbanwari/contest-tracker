ab82f77b3b8e53918310ba910894743477b53e89
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2020-09-25 21:39:38 -0700

    Modified API to support a request for contests from ALL sources in one go

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,29 @@ const codeforces = require('./codeforces');
 
 const supported = new Map();
 supported.set('cf', codeforces);
-app.get('/', async (req, res) => {
-    const data = await supported.get('cf')(5);
-    res.send(data);
+app.get('/api/contests', async (req, res) => {
+    promises = [];
+    supported.forEach((value, key) => {
+        const data = value(5);
+        promises.push(data);
+    });
+    body = "";
+    for (i = 0; i < promises.length; i++) {
+        const data = await promises[i];
+        body += JSON.stringify(data);
+    }
+    res.send(body);
 });
+app.get('/api/contests/:name', async (req, res) => {
+    if (!supported.has(req.params.name)) {
+        res.status(404).send("This source doesn't exist!");
+        return;
+    }
+    const data = await supported.get(req.params.name)(5).catch(() => {
+        throw new Error('Something broke!');
+    });
+    res.send(data);
+})
 
 
 
